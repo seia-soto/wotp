31875eb795eb0634f029023fe404b533db5f3101
Seia-Soto <seia@outlook.kr>
2021-06-17 17:16:39 +0900

    feat: add clean option to database initialization

--- src/database/init.js
+++ src/database/init.js
@@ -2,13 +2,16 @@ import knex from './knex'
 
 import * as tables from './tables'
 
-export default async () => {
+export default async clean => {
   const tableNames = Object.keys(tables)
 
   for (let i = 0, l = tableNames.length; i < l; i++) {
     const name = tableNames[i]
 
-    if (!await knex.schema.hasTable(name)) {
+    if (clean) {
+      await knex.schema.dropTableIfExists(name)
+    }
+    if (clean || !await knex.schema.hasTable(name)) {
       await knex.schema.createTable(name, tables[name])
     }
   }
